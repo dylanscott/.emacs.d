4b9b4a4bf1016317ee7e501462afde3337dc3533
Dylan Scott <dscott@hex.tech>
2023-04-07 23:21:09 -0700

    copypasta

---
+++ init.el
@@ -0,0 +1,42 @@
+;; Performance tweaking for modern machines
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (* 1024 1024))
+
+;; Hide UI
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; Better default modes
+(electric-pair-mode t)
+(show-paren-mode 1)
+(setq-default indent-tabs-mode nil)
+(save-place-mode t)
+(savehist-mode t)
+(recentf-mode t)
+(global-auto-revert-mode t)
+
+;; Better default settings
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward
+      window-resize-pixelwise t
+      frame-resize-pixelwise t
+      load-prefer-newer t
+      backup-by-copying t
+      custom-file (expand-file-name "custom.el" user-emacs-directory))
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+(use-package emacs
+  :config
+  (require-theme 'modus-themes)
+  (load-theme 'modus-vivendi))
+
+(use-package vertico
+  :ensure t
+  :custom
+  (vertico-cycle t)
+  (read-buffer-completion-ignore-case t)
+  (read-file-name-completion-ignore-case t)
+  (completion-styles '(basic substring partial-completion flex))
+  :init
+  (vertico-mode))
